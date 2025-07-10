4ad1b88d28c544547fada8e6284eac5e6249d879
Nilantha Sanjeewa <nilanthasanjeewa101@gmail.com>
2025-07-10 18:46:01 +0530

    chatbot answer speech option added to english and tamil option.

--- e-prescription-frontend/src/pages/Chatbot/Database Integration/medicine_details.py
+++ e-prescription-frontend/src/pages/Chatbot/Database Integration/medicine_details.py
@@ -6,7 +6,7 @@ import os
 
 # --- Configuration ---
 # IMPORTANT: Replace with the actual path to your Firebase service account key
-SERVICE_ACCOUNT_KEY_PATH = '../API_keys/medisl-ed07f-firebase-adminsdk-fbsvc-b70dc11484.json'
+SERVICE_ACCOUNT_KEY_PATH = '../API_keys/medisl-ed07f-firebase-adminsdk-fbsvc-2fe43ec477.json'
 CSV_FILE_PATH = '../Dataset/diseases_medicines.csv'
 MEDICINE_DETAILS_COLLECTION = 'medicine_details' # New collection name for medicine details
 
